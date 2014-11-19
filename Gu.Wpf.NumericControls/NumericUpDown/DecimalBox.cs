88eab5baaaac6b3c3785a99ff59bab041173ca8c
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-19 11:31:21 +0100

    Testing binding validation

--- Gu.Wpf.NumericControls/NumericUpDown/DecimalBox.cs
+++ Gu.Wpf.NumericControls/NumericUpDown/DecimalBox.cs
@@ -8,7 +8,7 @@
     {
         static DecimalBox()
         {
-            UpdateMetadata(typeof(DecimalBox), 1m, decimal.MinValue, decimal.MaxValue, 2);
+            UpdateMetadata(typeof(DecimalBox), 1m);
         }
 
         public DecimalBox()
@@ -16,35 +16,6 @@
             (x, y) => x + y,
             (x, y) => x - y)
         {
-        }
-
-        protected override void ValidateText(string txt)
-        {
-            decimal result;
-            if (decimal.TryParse(txt, out result))
-            {
-                this.Value = result;
-            }
-            else
-            {
-                this.ConvertValueToText();
-            }
-        }
-
-        protected override string ValidInput()
-        {
-            return @"^[-]?[\d]*[,.]?[\d]{0," + this.Decimals.ToString() + "}?$";
-        }
-
-        protected override bool ValidValue(string text)
-        {
-            return this.ValidateValue(Convert.ToDecimal(text))
-                .ToString() == text;
-        }
-
-        protected override void ConvertValueToText()
-        {
-            this.TextBox.Text = string.Format("{0:0." + new string('0', this.Decimals) + "} {1}", Math.Round(this.Value, this.Decimals), this.Suffix);
         }
     }
 }
---
+++ Gu.Wpf.NumericControls/NumericUpDown/ValueChangedEventArgs.cs
@@ -0,0 +1,30 @@
+﻿namespace Gu.Wpf.NumericControls
+{
+    using System.Windows;
+    public delegate void ValueChangedEventHandler<T>(object sender, ValueChangedEventArgs<T> e);
+    public class ValueChangedEventArgs<T> : RoutedEventArgs
+    {
+        public ValueChangedEventArgs(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        public ValueChangedEventArgs(T oldValue, T newValue, RoutedEvent routedEvent)
+            : base(routedEvent)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        public ValueChangedEventArgs(T oldValue, T newValue, RoutedEvent routedEvent, object source)
+            : base(routedEvent, source)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        public T OldValue { get; private set; }
+        public T NewValue { get; private set; }
+    }
+}
