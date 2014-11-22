379e4b638c0fb9eed91a2cfa47caccec2be745b9
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-22 15:05:29 +0100

    ~Cleaned up~ demo, not very happy with it.

--- Gu.Wpf.NumericInput.Tests/FloatBaseTests.cs
+++ Gu.Wpf.NumericInput.Tests/FloatBaseTests.cs
@@ -34,13 +34,25 @@ namespace Gu.Wpf.NumericInput.Tests
             Assert.AreEqual(expected, Sut.Text);
         }
 
-        [TestCase(2, "1.234", "1.23", 1.234f)]
-        public void ValueNotAffectedByDecimals(int decimals, string text, string expectedText, T expected)
+        [TestCase(2, "1.234", "1.23", 1.234)]
+        public void ValueNotAffectedByDecimalDigits(int decimals, string text, string expectedText, T expected)
         {
             Sut.Text = text;
             Sut.DecimalDigits = decimals;
             Assert.AreEqual(expectedText, Sut.Text);
             Assert.AreEqual(expected, Sut.Value);
+        }
+
+        [TestCase("1.234", 1.234, "1.23", 1.23)]
+        public void ValueUpdatedOnFewerDecimalDigitsFromUser(string text1, T expected1, string text2, T expected2)
+        {
+            Sut.DecimalDigits = 5;
+
+            Sut.Text = text1;
+            Assert.AreEqual(expected1, Sut.Value);
+
+            Sut.Text = text2;
+            Assert.AreEqual(expected2, Sut.Value);
         }
 
         [TestCase("1.234", 2, "1.23", 4, "1.2340")]
@@ -52,6 +64,25 @@ namespace Gu.Wpf.NumericInput.Tests
 
             Sut.DecimalDigits = decimals2;
             Assert.AreEqual(expected2, Sut.Text);
+        }
+
+
+        [Test]
+        public void AddedDigitsNotTruncated()
+        {
+            Sut.DecimalDigits = 2;
+            Sut.Text = "1.23";
+            Sut.Text = "1.234";
+            Assert.AreEqual(1.234, Sut.Value);
+        }
+
+        [Test]
+        public void FewerDecimalsUpdatesValue()
+        {
+            Sut.DecimalDigits = 4;
+            Sut.Text = "1.2334";
+            Sut.Text = "1.23";
+            Assert.AreEqual(1.23, Sut.Value);
         }
     }
 }
