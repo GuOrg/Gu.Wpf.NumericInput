fc86cab97e571214e16f1a506d415cad1b51fa2b
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-22 14:42:13 +0100

    Cleaned up or made a bigger mess of the sample.

---
+++ Gu.Wpf.NumericInput.Demo/IBoxVm.cs
@@ -0,0 +1,24 @@
+﻿namespace Gu.Wpf.NumericInput.Demo
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+
+    public interface IBoxVm : INotifyPropertyChanged
+    {
+        Type Type { get; }
+        bool Configurable { get; }
+
+        CultureInfo[] Cultures { get; }
+        CultureInfo Culture { get; set; }
+        IFormattable Min { get; set; }
+        IFormattable Max { get; set; }
+        IFormattable Value { get; set; }
+        int? DecimalDigits { get; set; }
+        bool AllowSpinners { get; set; }
+        bool IsReadonly { get; set; }
+        string Suffix { get; set; }
+        string RegexPattern { get; set; }
+        IFormattable Increment { get; set; }
+    }
+}
---
+++ Gu.Wpf.NumericInput.Demo/MainVm.cs
@@ -0,0 +1,44 @@
+﻿namespace Gu.Wpf.NumericInput.Demo
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using Gu.Wpf.NumericInput.Demo.Annotations;
+
+    public class MainVm : INotifyPropertyChanged
+    {
+        private readonly ObservableCollection<IBoxVm> _vms = new ObservableCollection<IBoxVm>();
+
+        public MainVm()
+        {
+            _vms = new ObservableCollection<IBoxVm>
+                       {
+                           new BoxVm<double>(typeof(DoubleBox)),
+                           new BoxVm<double>(typeof(DoubleBox), -10, 10,1),
+                           new BoxVm<int>(typeof(IntBox)),
+                           new BoxVm<int>(typeof(IntBox), -10, 10,1)
+                       };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<IBoxVm> Vms
+        {
+            get
+            {
+                return _vms;
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
