75b05455a148057bd29ca8b1e529f11ae65418d8
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-22 01:59:11 +0100

    Typed validation errors

---
+++ Gu.Wpf.NumericInput/Validation/CanParseValidationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Gu.Wpf.NumericInput.Validation
+{
+    using System;
+    using System.Windows.Controls;
+
+    public class CanParseValidationResult : ValidationResult
+    {
+        public CanParseValidationResult(Type type, String text, bool isValid, object errorContent)
+            : base(isValid, errorContent)
+        {
+            Type = type;
+            Text = text;
+        }
+        public Type Type { get; private set; }
+        public string Text { get; private set; }
+    }
+}
