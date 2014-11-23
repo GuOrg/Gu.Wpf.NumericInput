0c8380131ee7a5131f03b38af355f8e3882e20ef
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-23 10:49:25 +0100

    Broken test for undo.

---
+++ Gu.Wpf.NumericInput.Tests/Helpers/UiTestHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace Gu.Wpf.NumericInput.Tests
+{
+    using System.Windows;
+
+    public static class UiTestHelper
+    {
+        public static void Initialize<T>(this T element)
+            where T : FrameworkElement
+        {
+            element.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            element.Arrange(new Rect(element.DesiredSize));
+            element.UpdateLayout();
+        }
+
+
+        public static void Invalidate<T>(T element)
+    where T : FrameworkElement
+        {
+            element.InvalidateVisual();
+        }
+
+        public static Window ShowInWindow<T>(this T element)
+            where T : FrameworkElement
+        {
+            var window = new Window
+            {
+                Content = element
+            };
+            window.Show();
+            return window;
+        }
+        public static Window ShowInWindow<T>(this T element, object dataContext)
+    where T : FrameworkElement
+        {
+            var window = new Window()
+            {
+                Content = element
+            };
+            window.DataContext = dataContext;
+            window.Show();
+            return window;
+        }
+    }
+}
