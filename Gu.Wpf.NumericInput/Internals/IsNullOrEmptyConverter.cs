ac4fe58ea1738443a7cda5158496874f160117d4
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-27 16:54:28 +0100

    Add UITests.

---
+++ Gu.Wpf.NumericInput/Internals/IsNullOrEmptyConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace Gu.Wpf.NumericInput
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    internal class IsNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
