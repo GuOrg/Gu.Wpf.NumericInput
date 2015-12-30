d5a70c26a5d813c4cd699ea0e19f82c490942c33
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-30 16:44:26 +0100

    FormatDirtyEvent and ValidationDirtyEvent

---
+++ Gu.Wpf.NumericInput/BaseBoxes/BaseBox.Events.cs
@@ -0,0 +1,34 @@
+﻿namespace Gu.Wpf.NumericInput
+{
+    using System.Windows;
+
+    public abstract partial class BaseBox
+    {
+        internal static readonly RoutedEvent FormatDirtyEvent = EventManager.RegisterRoutedEvent(
+            "FormatDirty",
+            RoutingStrategy.Direct,
+            typeof(RoutedEventHandler),
+            typeof(BaseBox));
+
+        internal static readonly RoutedEvent ValidationDirtyEvent = EventManager.RegisterRoutedEvent(
+            "ValidationDirty",
+            RoutingStrategy.Direct,
+            typeof(RoutedEventHandler),
+            typeof(BaseBox));
+
+        protected static readonly RoutedEventArgs ValidationDirtyEventArgs = new RoutedEventArgs(ValidationDirtyEvent);
+        protected static readonly RoutedEventArgs FormatDirtyEventArgs = new RoutedEventArgs(FormatDirtyEvent);
+
+        internal event RoutedEventHandler FormatDirty
+        {
+            add { this.AddHandler(FormatDirtyEvent, value); }
+            remove { this.RemoveHandler(FormatDirtyEvent, value); }
+        }
+
+        internal event RoutedEventHandler ValidationDirty
+        {
+            add { this.AddHandler(ValidationDirtyEvent, value); }
+            remove { this.RemoveHandler(ValidationDirtyEvent, value); }
+        }
+    }
+}
