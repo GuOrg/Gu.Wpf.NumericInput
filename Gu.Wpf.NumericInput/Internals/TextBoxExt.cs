ff79521b02ab2baf33286d0bb7baff06091f83b6
Johan Larsson <johan_gavle1@hotmail.com>
2016-01-06 12:39:23 +0100

    Cleanup Spinners.

---
+++ Gu.Wpf.NumericInput/Internals/TextBoxExt.cs
@@ -0,0 +1,16 @@
+﻿namespace Gu.Wpf.NumericInput
+{
+    using System.Windows.Controls;
+
+    internal static class TextBoxExt
+    {
+        internal static void SetTextUndoable(this TextBox textBox, string text)
+        {
+            // http://stackoverflow.com/questions/27083236/change-the-text-in-a-textbox-with-text-binding-sometext-so-it-is-undoable/27083548?noredirect=1#comment42677255_27083548
+            // Dunno if nice, testing it for now
+            textBox.SelectAll();
+            textBox.SelectedText = text;
+            textBox.Select(0, 0);
+        }
+    }
+}
