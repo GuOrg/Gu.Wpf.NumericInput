4fc3f845557f48e1e8fb8315ae829d7c73b5f9f1
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-22 12:25:44 +0200

    Support touch.

---
+++ Gu.Wpf.NumericInput/Touch/TouchKeyboard.cs
@@ -0,0 +1,61 @@
+﻿namespace Gu.Wpf.NumericInput.Touch
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Windows.Input;
+
+    public static class TouchKeyboard
+    {
+        private static TouchProcessInfo touchProcessInfo = TouchProcessInfo.Default;
+
+        public static string TouchKeyboardPath
+        {
+            get { return touchProcessInfo?.ProcessStartInfo.FileName; }
+            set { touchProcessInfo = TouchProcessInfo.Create(value); }
+        }
+
+        public static void Show()
+        {
+            if (touchProcessInfo?.ProcessStartInfo == null)
+            {
+                return;
+            }
+
+            NumericInput.Debug.WriteLine("show");
+            Process.Start(touchProcessInfo.ProcessStartInfo);
+        }
+
+        public static void Hide()
+        {
+            if (touchProcessInfo?.ProcessStartInfo == null)
+            {
+                return;
+            }
+
+            // http://mheironimus.blogspot.se/2015/05/adding-touch-keyboard-support-to-wpf.html
+            var keyboardWnd = FindWindow("IPTip_Main_Window", null);
+            var nullIntPtr = new IntPtr(0);
+            const uint WmSyscommand = 0x0112;
+            var scClose = new IntPtr(0xF060);
+
+            if (keyboardWnd != nullIntPtr)
+            {
+                NumericInput.Debug.WriteLine("hide");
+                SendMessage(keyboardWnd, WmSyscommand, scClose, nullIntPtr);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindow(string sClassName, string sAppName);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+        private static bool HasTouchInput()
+        {
+            return Tablet.TabletDevices.Cast<TabletDevice>().Any(tabletDevice => tabletDevice.Type == TabletDeviceType.Touch);
+        }
+    }
+}
---
+++ Gu.Wpf.NumericInput/Touch/TouchProcessInfo.cs
@@ -0,0 +1,51 @@
+﻿namespace Gu.Wpf.NumericInput.Touch
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    internal class TouchProcessInfo
+    {
+        private TouchProcessInfo(string path)
+        {
+            this.ProcessStartInfo = new ProcessStartInfo(path);
+            this.ProcessName = Path.GetFileNameWithoutExtension(path);
+        }
+
+        /// <summary>
+        /// Gets the default TouchProcessInfo pointing to C:\Program Files\Common Files\Microsoft Shared\Ink\TabTip.exe
+        /// </summary>
+        public static TouchProcessInfo Default { get; } = CreateDefault();
+
+        public ProcessStartInfo ProcessStartInfo { get; }
+
+        public string ProcessName { get; }
+
+        public static TouchProcessInfo Create(string path)
+        {
+            if (path == null || Path.GetExtension(path) != ".exe")
+            {
+                return null;
+            }
+
+            if (File.Exists(path))
+            {
+                return new TouchProcessInfo(path);
+            }
+
+            return null;
+        }
+
+        private static TouchProcessInfo CreateDefault()
+        {
+            const string microsoftSharedInkTabtipExe = @"Microsoft Shared\ink\TabTip.exe";
+
+            return
+                Create(@"C:\Program Files\Common Files\Microsoft Shared\Ink\TabTip.exe") ??
+                Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles),
+                    microsoftSharedInkTabtipExe)) ??
+                Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+                    microsoftSharedInkTabtipExe));
+        }
+    }
+}
