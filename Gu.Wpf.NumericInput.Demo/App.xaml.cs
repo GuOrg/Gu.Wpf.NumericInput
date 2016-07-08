d808f39c482d72d167de23b0a73048be3c6698a5
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-08 17:22:21 +0200

    Start refactoring UI-tests.

--- Gu.Wpf.NumericInput.Demo/App.xaml.cs
+++ Gu.Wpf.NumericInput.Demo/App.xaml.cs
@@ -1,11 +1,19 @@
 ﻿namespace Gu.Wpf.NumericInput.Demo
 {
+    using System;
     using System.Windows;
 
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (e.Args.Length == 1)
+            {
+                var window = e.Args[0];
+                this.StartupUri = new Uri($"UiTestWindows/{window}.xaml", UriKind.Relative);
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
---
+++ Gu.Wpf.NumericInput.Demo/UiTestWindows/FocusWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.NumericInput.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    public partial class FocusWindow : Window
+    {
+        public FocusWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
