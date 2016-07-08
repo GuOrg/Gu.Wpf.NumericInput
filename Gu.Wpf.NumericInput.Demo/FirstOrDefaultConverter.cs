d71d3cae049c33b9c77abc983878fad985842823
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-08 19:38:59 +0200

    Refactor `DoubleBox` validation tests to separate window WIP.

---
+++ Gu.Wpf.NumericInput.Demo/FirstOrDefaultConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Gu.Wpf.NumericInput.Demo
+{
+    using System;
+    using System.Collections;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows.Data;
+
+    public class FirstOrDefaultConverter : IValueConverter
+    {
+        public static readonly FirstOrDefaultConverter Default = new FirstOrDefaultConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = value as IEnumerable;
+            return enumerable?.Cast<object>()
+                              .FirstOrDefault();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gu.Wpf.NumericInput.Demo/UiTestWindows/DoubleBoxValidationWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.NumericInput.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    public partial class DoubleBoxValidationWindow : Window
+    {
+        public DoubleBoxValidationWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
