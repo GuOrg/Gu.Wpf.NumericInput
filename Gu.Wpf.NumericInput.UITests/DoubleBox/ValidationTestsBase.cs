69e7fcf127e74ada24a194675d4e8f2a32a52f95
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-09 15:26:15 +0200

    Happy path tests for sv-SE

--- Gu.Wpf.NumericInput.UITests/DoubleBox/ValidationTestsBase.cs
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/ValidationTestsBase.cs
@@ -8,7 +8,13 @@
         protected Button LoseFocusButton;
         protected TextBox ViewModelValueBox;
         protected ComboBox CultureBox;
+
+        protected CheckBox AllowLeadingWhiteBox;
+        protected CheckBox AllowTrailingWhiteBox;
+        protected CheckBox AllowLeadingSignBox;
         protected CheckBox AllowDecimalPointBox;
+        protected CheckBox AllowThousandsBox;
+        protected CheckBox AllowExponentBox;
 
         protected override string WindowName { get; } = "DoubleBoxValidationWindow";
 
@@ -18,7 +24,12 @@
             this.LoseFocusButton = this.Window.GetByText<Button>("lose focus");
             this.ViewModelValueBox = this.Window.Get<TextBox>("ViewModelValue");
             this.CultureBox = this.Window.Get<ComboBox>("Culture");
+            this.AllowLeadingWhiteBox = this.Window.Get<CheckBox>("AllowLeadingWhite");
+            this.AllowTrailingWhiteBox = this.Window.Get<CheckBox>("AllowTrailingWhite");
+            this.AllowLeadingSignBox = this.Window.Get<CheckBox>("AllowLeadingSign");
             this.AllowDecimalPointBox = this.Window.Get<CheckBox>("AllowDecimalPoint");
+            this.AllowThousandsBox = this.Window.Get<CheckBox>("AllowThousands");
+            this.AllowExponentBox = this.Window.Get<CheckBox>("AllowExponent");
         }
     }
 }
