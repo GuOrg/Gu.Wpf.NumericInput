9bf56a63656532cd2888f8fab2d5c56114a74bf3
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-09 14:59:23 +0200

    Add some happy paths tests for DoubleBox validation.

---
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/ValidationTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Gu.Wpf.NumericInput.UITests.DoubleBox
+{
+    using TestStack.White.UIItems;
+    using TestStack.White.UIItems.ListBoxItems;
+
+    public class ValidationTestsBase : WindowTests
+    {
+        protected Button LoseFocusButton;
+        protected TextBox ViewModelValueBox;
+        protected ComboBox CultureBox;
+        protected CheckBox AllowDecimalPointBox;
+
+        protected override string WindowName { get; } = "DoubleBoxValidationWindow";
+
+        public override void OneTimeSetUp()
+        {
+            base.OneTimeSetUp();
+            this.LoseFocusButton = this.Window.GetByText<Button>("lose focus");
+            this.ViewModelValueBox = this.Window.Get<TextBox>("ViewModelValue");
+            this.CultureBox = this.Window.Get<ComboBox>("Culture");
+            this.AllowDecimalPointBox = this.Window.Get<CheckBox>("AllowDecimalPoint");
+        }
+    }
+}
