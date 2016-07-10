e873a26697c62713e3a49fa6f469b6ad2b6b85b9
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-10 16:11:15 +0200

    Refactor IsGreaterThanValidationResult
    - Add tests for string formats.

---
+++ Gu.Wpf.NumericInput/Validation/ValidationResults/FormatAndCulture/FormatAndCulture.cs
@@ -0,0 +1,48 @@
+﻿namespace Gu.Wpf.NumericInput
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Globalization;
+
+    public abstract class FormatAndCulture<T> : IFormatAndCulture
+        where T : IFormatAndCulture
+    {
+        protected FormatAndCulture(IFormatProvider formatProvider, string resourceKey)
+        {
+            this.FormatProvider = formatProvider;
+            this.ResourceKey = resourceKey;
+            this.Format = this.GetFormat(formatProvider);
+        }
+
+        /// <summary>The name of the resource. I.e. Properties.Resources.ResourceManager.GetString(<see cref="ResourceKey"/>, <see cref="CultureInfo"/>)</summary>
+        public string ResourceKey { get; }
+
+        /// <summary>The culture for which the <see cref="Format"/> is for. If no localization is found <see cref="CultureInfo.InvariantCulture"/> will be used.</summary>
+        public IFormatProvider FormatProvider { get; }
+
+        /// <summary>Gets the localized format string.</summary>
+        public string Format { get; }
+
+        protected ConcurrentDictionary<CultureInfo, T> Cache { get; } = new ConcurrentDictionary<CultureInfo, T>();
+
+        public abstract T GetOrCreate(IFormatProvider formatProvider);
+
+        public string GetFormat(IFormatProvider culture)
+        {
+            var format = Properties.Resources.ResourceManager.GetString(this.ResourceKey, culture as CultureInfo);
+            if (!string.IsNullOrEmpty(format))
+            {
+                this.AssertFormat(culture, format);
+                return format;
+            }
+
+            return Properties.Resources.ResourceManager.GetString(this.ResourceKey, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>Assert that <paramref name="format"/> is valid.</summary>
+        /// <param name="culture">The culture.</param>
+        /// <param name="format">The format string.</param>
+        /// <exception cref="InvalidOperationException">If the format does not match the expected.</exception>
+        protected abstract void AssertFormat(IFormatProvider culture, string format);
+    }
+}
---
+++ Gu.Wpf.NumericInput/Validation/ValidationResults/FormatAndCulture/OneParameterFormatAndCulture.cs
@@ -0,0 +1,58 @@
+﻿namespace Gu.Wpf.NumericInput
+{
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+
+    public class OneParameterFormatAndCulture : FormatAndCulture<OneParameterFormatAndCulture>
+    {
+        private OneParameterFormatAndCulture(IFormatProvider formatProvider, string resourceKey)
+            : base(formatProvider, resourceKey)
+        {
+        }
+
+        public string FormatMessage(IFormattable arg)
+        {
+            return string.Format(this.FormatProvider, this.Format, arg);
+        }
+
+        /// <summary>Create a <see cref="OneParameterFormatAndCulture"/> for a resource in <see cref="Gu.Wpf.NumericInput.Properties.Resources"/>.</summary>
+        /// <param name="resourceKey">A key in <see cref="Gu.Wpf.NumericInput.Properties.Resources"/></param>
+        /// <returns>A <see cref="OneParameterFormatAndCulture"/> that can be used for formatting error messages.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If <paramref name="resourceKey"/> is not found in <see cref="Gu.Wpf.NumericInput.Properties.Resources"/>.</exception>
+        public static OneParameterFormatAndCulture CreateDefault(string resourceKey)
+        {
+            if (typeof(Properties.Resources).GetProperty(resourceKey, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic) == null)
+            {
+                throw new ArgumentOutOfRangeException($"No resource found for key: {resourceKey}");
+            }
+
+            var formatAndCulture = new OneParameterFormatAndCulture(CultureInfo.InvariantCulture, resourceKey);
+            formatAndCulture.Cache[CultureInfo.InvariantCulture] = formatAndCulture;
+            return formatAndCulture;
+        }
+
+        /// <inheritdoc/>
+        public override OneParameterFormatAndCulture GetOrCreate(IFormatProvider formatProvider)
+        {
+            var culture = formatProvider as CultureInfo ?? CultureInfo.InvariantCulture;
+            return this.Cache.GetOrAdd(culture, c => new OneParameterFormatAndCulture(formatProvider, this.ResourceKey));
+        }
+
+        /// <inheritdoc/>
+        protected override void AssertFormat(IFormatProvider culture, string format)
+        {
+            int itemCount;
+            bool? _;
+            if (!FormatString.IsValidFormat(format, out itemCount, out _))
+            {
+                throw new InvalidOperationException($"The format: '{format}' for culture: '{culture}' is not valid.");
+            }
+
+            if (itemCount != 1)
+            {
+                throw new InvalidOperationException($"The format: '{format}' for culture: '{culture}' is not for one argument as expected.");
+            }
+        }
+    }
+}
