93ce1cb57a03cd83bce8f539a7c12ef934dff905
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-11 06:19:33 +0200

    Please enter a value between when bot min & max.

--- Gu.Wpf.NumericInput/Properties/Resources.Designer.cs
+++ Gu.Wpf.NumericInput/Properties/Resources.Designer.cs
@@ -70,6 +70,15 @@ namespace Gu.Wpf.NumericInput.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Please enter a value between {0} and {1}..
+        /// </summary>
+        public static string Please_enter_a_value_between__0__and__1__ {
+            get {
+                return ResourceManager.GetString("Please_enter_a_value_between__0__and__1__", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Please enter a value greater than or equal to {0}..
         /// </summary>
         public static string Please_enter_a_value_greater_than_or_equal_to__0__ {
