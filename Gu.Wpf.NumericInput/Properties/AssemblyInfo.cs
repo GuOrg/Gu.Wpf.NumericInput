94b55183ebfbead69049c1a9dc4a5426fe526ac8
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-30 09:20:55 +0200

    Bump version.

--- Gu.Wpf.NumericInput/Properties/AssemblyInfo.cs
+++ Gu.Wpf.NumericInput/Properties/AssemblyInfo.cs
@@ -5,9 +5,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Gu.Wpf.NumericInput")]
 [assembly: AssemblyDescription("WPF textboxes for numeric input")]
 [assembly: AssemblyConfiguration("")]
@@ -16,34 +13,11 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// In order to begin building localizable applications, set
-// <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-// inside a <PropertyGroup>.  For example, if you are using US english
-// in your source files, set the <UICulture> to en-US.  Then uncomment
-// the NeutralResourceLanguage attribute below.  Update the "en-US" in
-// the line below to match the UICulture setting in the project file.
-
-// [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.1.1")]
-[assembly: AssemblyFileVersion("0.5.1.1")]
+[assembly: AssemblyVersion("0.5.1.2")]
+[assembly: AssemblyFileVersion("0.5.1.2")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: Guid("2026010E-5005-413E-BA3F-18CF88F28B0F")]
 [assembly: InternalsVisibleTo("Gu.Wpf.NumericInput.Tests", AllInternalsVisible = true)]
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 0.5.1.2
+* BUGFIX: Use type IFormatProvider in CLR accessors for NumericBox.Culture.
+
 #### 0.5.1.1
 * BUGFIX: Don't throw NRE in marginconverter.
