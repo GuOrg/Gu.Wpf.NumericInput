29ebd137d6ab41c62dbb56ae39b92136545c8bee
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-30 07:48:21 +0200

    Use IFormatProvider in CLR accessors for NumericBox.Culture

---
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/DefaultCultureWindowTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Gu.Wpf.NumericInput.UITests.DoubleBox
+{
+    using NUnit.Framework;
+    using TestStack.White.UIItems;
+    using TestStack.White.WindowsAPI;
+
+    public class DefaultCultureWindowTests : WindowTests
+    {
+        protected override string WindowName { get; } = "DefaultCultureWindow";
+
+        private TextBox ValueTextBox => this.GetCachedTextBox();
+
+        private TextBox SpinnerDoubleBox => this.GetCachedTextBox();
+
+        private TextBox DoubleBox => this.GetCachedTextBox();
+
+        [Test]
+        public void OnLoad()
+        {
+            this.ValueTextBox.BulkText = "1.234";
+            this.Window.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.TAB);
+            Assert.AreEqual("1.234", this.SpinnerDoubleBox.Text);
+            Assert.AreEqual("1.234", this.DoubleBox.Text);
+        }
+    }
+}
