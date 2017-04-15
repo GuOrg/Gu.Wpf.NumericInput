bec63539ead41efad7d1ae570731f41965a774eb
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-15 21:33:01 +0200

    [assembly:NUnit.Framework.LevelOfParallelism(1)]

--- Gu.Wpf.NumericInput.UITests/Properties/AssemblyInfo.cs
+++ Gu.Wpf.NumericInput.UITests/Properties/AssemblyInfo.cs
@@ -12,3 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("03820c66-202b-4b6d-b9d5-e4f3267362c1")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NUnit.Framework.LevelOfParallelism(1)]
