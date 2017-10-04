5d9e38b074b37d87b3e8f87b51cc82ca79a29035
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-04 09:47:10 +0200

    Refactor tests.

---
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/CycleFocusWindowTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Gu.Wpf.NumericInput.UITests.DoubleBox
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public sealed class CycleFocusWindowTests
+    {
+        private const string WindowName = "CycleFocusWindow";
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Application.KillLaunched(Application.FindExe("Gu.Wpf.NumericInput.Demo.exe"));
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void WithSpinners(bool withSpinners)
+        {
+            using (var application = Application.AttachOrLaunch(Application.FindExe("Gu.Wpf.NumericInput.Demo.exe"), WindowName))
+            {
+                var window = application.MainWindow;
+                var doubleBoxes = window.FindGroupBox("DoubleBoxes");
+                var textBox = doubleBoxes.FindTextBox("TextBox1");
+                textBox.Click();
+                window.FindGroupBox("Settings").FindCheckBox("AllowSpinners").IsChecked = withSpinners;
+                var doubleBox1 = doubleBoxes.FindTextBox("DoubleBox1");
+                var doubleBox2 = doubleBoxes.FindTextBox("DoubleBox2");
+                var doubleBox3 = doubleBoxes.FindTextBox("DoubleBox3");
+
+                textBox.Click();
+                Assert.AreEqual(true, textBox.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox3.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual(false, textBox.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox3.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual(false, textBox.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox2.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox3.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual(false, textBox.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox3.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                window.WaitUntilResponsive();
+                Assert.AreEqual(true, textBox.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox3.HasKeyboardFocus);
+            }
+        }
+    }
+}
---
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/FocusWindowTests.cs
@@ -0,0 +1,108 @@
+﻿namespace Gu.Wpf.NumericInput.UITests.DoubleBox
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public sealed class FocusWindowTests
+    {
+        private const string WindowName = "FocusWindow";
+
+        [SetUp]
+        public void SetUp()
+        {
+            using (var app = Application.AttachOrLaunch(Application.FindExe("Gu.Wpf.NumericInput.Demo.exe"), WindowName))
+            {
+                var window = app.MainWindow;
+                window.FindTextBox("TextBox2").Enter("2.345");
+                window.FindCheckBox("AllowSpinnersBox").IsChecked = false;
+                window.WaitUntilResponsive();
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Application.KillLaunched(Application.FindExe("Gu.Wpf.NumericInput.Demo.exe"));
+        }
+
+        [Test]
+        public void NoSpinnersNoSuffix()
+        {
+            using (var app = Application.AttachOrLaunch(Application.FindExe("Gu.Wpf.NumericInput.Demo.exe"), WindowName))
+            {
+                var window = app.MainWindow;
+                var doubleBoxes = window.FindGroupBox("DoubleBoxes");
+                var textBoxes = window.FindGroupBox("TextBoxes");
+                var textBox1 = doubleBoxes.FindTextBox("TextBox1");
+                var doubleBox1 = doubleBoxes.FindTextBox("DoubleBox1");
+                var doubleBox2 = doubleBoxes.FindTextBox("DoubleBox2");
+                doubleBox1.Click();
+
+                Assert.AreEqual(false, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+                doubleBox1.Enter("2");
+                Assert.AreEqual("2.345", textBoxes.FindTextBox("TextBox2").Text);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual("2", textBoxes.FindTextBox("TextBox2").Text);
+                Assert.AreEqual(false, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox2.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual(true, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                window.WaitUntilResponsive();
+                Assert.AreEqual(false, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+            }
+        }
+
+        [Test]
+        public void WithSpinners()
+        {
+            using (var app = Application.AttachOrLaunch(Application.FindExe("Gu.Wpf.NumericInput.Demo.exe"), WindowName))
+            {
+                var window = app.MainWindow;
+                var doubleBoxes = window.FindGroupBox("DoubleBoxes");
+                var textBoxes = window.FindGroupBox("TextBoxes");
+                var textBox1 = doubleBoxes.FindTextBox("TextBox1");
+                var doubleBox1 = doubleBoxes.FindTextBox("DoubleBox1");
+                var doubleBox2 = doubleBoxes.FindTextBox("DoubleBox2");
+                window.FindCheckBox("AllowSpinnersBox").IsChecked = true;
+                doubleBox1.Click();
+                Assert.AreEqual(false, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+                doubleBox1.Enter("2");
+                Assert.AreEqual("2.345", textBoxes.FindTextBox("TextBox2").Text);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual("2", textBoxes.FindTextBox("TextBox2").Text);
+                Assert.AreEqual(false, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox2.HasKeyboardFocus);
+                doubleBox1.IncreaseButton().Click();
+                Assert.AreEqual("3", doubleBox1.EditText());
+                Assert.AreEqual("2", textBoxes.FindTextBox("TextBox2").Text);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual("3", doubleBox1.EditText());
+                Assert.AreEqual("3", textBoxes.FindTextBox("TextBox2").Text);
+                Assert.AreEqual(false, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(true, doubleBox2.HasKeyboardFocus);
+
+                Keyboard.Type(Key.TAB);
+                Assert.AreEqual(true, textBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox1.HasKeyboardFocus);
+                Assert.AreEqual(false, doubleBox2.HasKeyboardFocus);
+            }
+        }
+    }
+}
