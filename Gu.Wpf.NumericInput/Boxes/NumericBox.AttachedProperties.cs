5c617b09c1c1ca3196df4332fa3b28675e9b2985
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-12 15:59:18 +0200

    Standard documentation text.

--- Gu.Wpf.NumericInput/Boxes/NumericBox.AttachedProperties.cs
+++ Gu.Wpf.NumericInput/Boxes/NumericBox.AttachedProperties.cs
@@ -1,4 +1,4 @@
-﻿namespace Gu.Wpf.NumericInput
+namespace Gu.Wpf.NumericInput
 {
     using System;
     using System.ComponentModel;
@@ -69,120 +69,92 @@
             typeof(NumericBox),
             new PropertyMetadata(default(string)));
 
-        /// <summary>
-        /// Helper for reading Culture property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read Culture property from.</param>
+        /// <summary>Helper for getting <see cref="CultureProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="CultureProperty"/> from.</param>
         /// <returns>Culture property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
         [TypeConverter(typeof(CultureInfoConverter))]
         public static IFormatProvider GetCulture(this UIElement element) => (IFormatProvider)element.GetValue(CultureProperty);
 
-        /// <summary>
-        /// Helper for setting Culture property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set Culture property on.</param>
+        /// <summary>Helper for setting <see cref="CultureProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="CultureProperty"/> on.</param>
         /// <param name="value">Culture property value.</param>
         public static void SetCulture(this UIElement element, IFormatProvider value) => element.SetValue(CultureProperty, value);
 
-        /// <summary>
-        /// Gets a value indicating when validation is performed, the default is LostFocus to be consistent with vanilla WPF TextBox
-        /// Setting ValidationTrigger="PropertyChanged" validates as you type even if the binding has UpdateSourceTrigger=LostFocus.
-        /// Inherits so can be set on for example a Window.
-        /// </summary>
+        /// <summary>Helper for getting <see cref="ValidationTriggerProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="ValidationTriggerProperty"/> from.</param>
+        /// <returns>ValidationTrigger property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
         public static ValidationTrigger GetValidationTrigger(this UIElement element) => (ValidationTrigger)element.GetValue(ValidationTriggerProperty);
 
-        /// <summary>
-        /// Gets a value indicating when validation is performed, the default is LostFocus to be consistent with vanilla WPF TextBox
-        /// Setting ValidationTrigger="PropertyChanged" validates as you type even if the binding has UpdateSourceTrigger=LostFocus.
-        /// Inherits so can be set on for example a Window.
-        /// </summary>
+        /// <summary>Helper for setting <see cref="ValidationTriggerProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="ValidationTriggerProperty"/> on.</param>
+        /// <param name="value">ValidationTrigger property value.</param>
         public static void SetValidationTrigger(this UIElement element, ValidationTrigger value) => element.SetValue(ValidationTriggerProperty, value);
 
-        /// <summary>
-        /// Helper for setting CanValueBeNull property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set CanValueBeNull property on.</param>
+        /// <summary>Helper for setting <see cref="CanValueBeNullProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="CanValueBeNullProperty"/> on.</param>
         /// <param name="value">CanValueBeNull property value.</param>
         public static void SetCanValueBeNull(this UIElement element, bool value) => element.SetValue(CanValueBeNullProperty, BooleanBoxes.Box(value));
 
-        /// <summary>
-        /// Helper for reading CanValueBeNull property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read CanValueBeNull property from.</param>
+        /// <summary>Helper for getting <see cref="CanValueBeNullProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="CanValueBeNullProperty"/> from.</param>
         /// <returns>CanValueBeNull property value.</returns>
         public static bool GetCanValueBeNull(this UIElement element) => (bool)element.GetValue(CanValueBeNullProperty);
 
-        /// <summary>
-        /// Helper for setting NumberStyles property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set NumberStyles property on.</param>
+        /// <summary>Helper for setting <see cref="NumberStylesProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="NumberStylesProperty"/> on.</param>
         /// <param name="value">NumberStyles property value.</param>
         public static void SetNumberStyles(this UIElement element, NumberStyles value) => element.SetValue(NumberStylesProperty, value);
 
-        /// <summary>
-        /// Helper for reading NumberStyles property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read NumberStyles property from.</param>
+        /// <summary>Helper for getting <see cref="NumberStylesProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="NumberStylesProperty"/> from.</param>
         /// <returns>NumberStyles property value.</returns>
         public static NumberStyles GetNumberStyles(this UIElement element) => (NumberStyles)element.GetValue(NumberStylesProperty);
 
-        /// <summary>
-        /// Helper for setting StringFormat property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set StringFormat property on.</param>
+        /// <summary>Helper for setting <see cref="StringFormatProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="StringFormatProperty"/> on.</param>
         /// <param name="value">StringFormat property value.</param>
         public static void SetStringFormat(this UIElement element, string value) => element.SetValue(StringFormatProperty, value);
 
-        /// <summary>
-        /// Helper for reading StringFormat property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read StringFormat property from.</param>
+        /// <summary>Helper for getting <see cref="StringFormatProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="StringFormatProperty"/> from.</param>
         /// <returns>StringFormat property value.</returns>
         public static string GetStringFormat(this UIElement element) => (string)element.GetValue(StringFormatProperty);
 
-        /// <summary>
-        /// Helper for setting DecimalDigits property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set DecimalDigits property on.</param>
+        /// <summary>Helper for setting <see cref="DecimalDigitsProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="DecimalDigitsProperty"/> on.</param>
         /// <param name="value">DecimalDigits property value.</param>
         public static void SetDecimalDigits(this UIElement element, int? value) => element.SetValue(DecimalDigitsProperty, value);
 
-        /// <summary>
-        /// Helper for reading DecimalDigits property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read DecimalDigits property from.</param>
+        /// <summary>Helper for getting <see cref="DecimalDigitsProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="DecimalDigitsProperty"/> from.</param>
         /// <returns>DecimalDigits property value.</returns>
         public static int? GetDecimalDigits(this UIElement element) => (int?)element.GetValue(DecimalDigitsProperty);
 
-        /// <summary>
-        /// Gets a value indicating whether spinners should be visible.
-        /// </summary>
+        /// <summary>Helper for getting <see cref="AllowSpinnersProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="AllowSpinnersProperty"/> from.</param>
+        /// <returns>AllowSpinners property value.</returns>
         public static bool GetAllowSpinners(this UIElement element) => (bool)element.GetValue(AllowSpinnersProperty);
 
-        /// <summary>
-        /// Sets a value indicating whether spinners should be visible.
-        /// </summary>
+        /// <summary>Helper for setting <see cref="AllowSpinnersProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="AllowSpinnersProperty"/> on.</param>
+        /// <param name="value">AllowSpinners property value.</param>
         public static void SetAllowSpinners(this UIElement element, bool value) => element.SetValue(AllowSpinnersProperty, BooleanBoxes.Box(value));
 
-        /// <summary>
-        /// Get a value that specifies how the IncreaseCommand and DecreaseCommand behaves.
-        /// The default is AsBinding meaning the value updates using the UpdateSourceTrigger specified in the binding. Default is LostFocus.
-        /// If set to PropertyChanged the binding source will be updated at each click even if the binding has UpdateSourceTrigger = LostFocus
-        /// </summary>
+        /// <summary>Helper for getting <see cref="SpinUpdateModeProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to read <see cref="SpinUpdateModeProperty"/> from.</param>
+        /// <returns>SpinUpdateMode property value.</returns>
         public static SpinUpdateMode GetSpinUpdateMode(DependencyObject element)
         {
             return (SpinUpdateMode)element.GetValue(SpinUpdateModeProperty);
         }
 
-        /// <summary>
-        /// Set a value that specifies how the IncreaseCommand and DecreaseCommand behaves.
-        /// The default is AsBinding meaning the value updates using the UpdateSourceTrigger specified in the binding. Default is LostFocus.
-        /// If set to PropertyChanged the binding source will be updated at each click even if the binding has UpdateSourceTrigger = LostFocus
-        /// </summary>
+        /// <summary>Helper for setting <see cref="SpinUpdateModeProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to set <see cref="SpinUpdateModeProperty"/> on.</param>
+        /// <param name="value">SpinUpdateMode property value.</param>
         public static void SetSpinUpdateMode(DependencyObject element, SpinUpdateMode value)
         {
             element.SetValue(SpinUpdateModeProperty, value);
