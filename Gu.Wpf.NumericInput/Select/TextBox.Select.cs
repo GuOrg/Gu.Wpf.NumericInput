591056637f48f81784ba19df421b831e2d9ea0a5
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-07 10:21:54 +0200

    Fix warnings.

--- Gu.Wpf.NumericInput/Select/TextBox.Select.cs
+++ Gu.Wpf.NumericInput/Select/TextBox.Select.cs
@@ -67,20 +67,16 @@ namespace Gu.Wpf.NumericInput.Select
             EventManager.RegisterClassHandler(typeof(TextBoxBase), Control.MouseDoubleClickEvent, new RoutedEventHandler(OnMouseDoubleClickSelectAllText));
         }
 
-        /// <summary>
-        /// Helper for setting SelectAllOnGotKeyboardFocus property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set SelectAllOnGotKeyboardFocus property on.</param>
+        /// <summary>Helper for setting <see cref="SelectAllOnGotKeyboardFocusProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="SelectAllOnGotKeyboardFocusProperty"/> on.</param>
         /// <param name="value">SelectAllOnGotKeyboardFocus property value.</param>
         public static void SetSelectAllOnGotKeyboardFocus(this UIElement element, bool value)
         {
             element.SetValue(SelectAllOnGotKeyboardFocusProperty, BooleanBoxes.Box(value));
         }
 
-        /// <summary>
-        /// Helper for reading SelectAllOnGotKeyboardFocus property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read SelectAllOnGotKeyboardFocus property from.</param>
+        /// <summary>Helper for getting <see cref="SelectAllOnGotKeyboardFocusProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="SelectAllOnGotKeyboardFocusProperty"/> from.</param>
         /// <returns>SelectAllOnGotKeyboardFocus property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
@@ -89,20 +85,16 @@ namespace Gu.Wpf.NumericInput.Select
             return Equals(BooleanBoxes.True, element.GetValue(SelectAllOnGotKeyboardFocusProperty));
         }
 
-        /// <summary>
-        /// Helper for setting SelectAllOnClick property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set SelectAllOnClick property on.</param>
+        /// <summary>Helper for setting <see cref="SelectAllOnClickProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="SelectAllOnClickProperty"/> on.</param>
         /// <param name="value">SelectAllOnClick property value.</param>
         public static void SetSelectAllOnClick(this UIElement element, bool value)
         {
             element.SetValue(SelectAllOnClickProperty, BooleanBoxes.Box(value));
         }
 
-        /// <summary>
-        /// Helper for reading SelectAllOnClick property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read SelectAllOnClick property from.</param>
+        /// <summary>Helper for getting <see cref="SelectAllOnClickProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="SelectAllOnClickProperty"/> from.</param>
         /// <returns>SelectAllOnClick property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
@@ -111,20 +103,16 @@ namespace Gu.Wpf.NumericInput.Select
             return Equals(BooleanBoxes.True, element.GetValue(SelectAllOnClickProperty));
         }
 
-        /// <summary>
-        /// Helper for setting SelectAllOnDoubleClick property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set SelectAllOnDoubleClick property on.</param>
+        /// <summary>Helper for setting <see cref="SelectAllOnDoubleClickProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="SelectAllOnDoubleClickProperty"/> on.</param>
         /// <param name="value">SelectAllOnDoubleClick property value.</param>
         public static void SetSelectAllOnDoubleClick(this UIElement element, bool value)
         {
             element.SetValue(SelectAllOnDoubleClickProperty, BooleanBoxes.Box(value));
         }
 
-        /// <summary>
-        /// Helper for reading SelectAllOnDoubleClick property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read SelectAllOnDoubleClick property from.</param>
+        /// <summary>Helper for getting <see cref="SelectAllOnDoubleClickProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="SelectAllOnDoubleClickProperty"/> from.</param>
         /// <returns>SelectAllOnDoubleClick property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
@@ -133,20 +121,16 @@ namespace Gu.Wpf.NumericInput.Select
             return Equals(BooleanBoxes.True, element.GetValue(SelectAllOnDoubleClickProperty));
         }
 
-        /// <summary>
-        /// Helper for setting MoveFocusOnEnter property on a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to set MoveFocusOnEnter property on.</param>
+        /// <summary>Helper for setting <see cref="MoveFocusOnEnterProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to set <see cref="MoveFocusOnEnterProperty"/> on.</param>
         /// <param name="value">MoveFocusOnEnter property value.</param>
         public static void SetMoveFocusOnEnter(this UIElement element, bool value)
         {
             element.SetValue(MoveFocusOnEnterProperty, BooleanBoxes.Box(value));
         }
 
-        /// <summary>
-        /// Helper for reading MoveFocusOnEnter property from a UIElement.
-        /// </summary>
-        /// <param name="element">UIElement to read MoveFocusOnEnter property from.</param>
+        /// <summary>Helper for getting <see cref="MoveFocusOnEnterProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="UIElement"/> to read <see cref="MoveFocusOnEnterProperty"/> from.</param>
         /// <returns>MoveFocusOnEnter property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
@@ -155,20 +139,16 @@ namespace Gu.Wpf.NumericInput.Select
             return Equals(BooleanBoxes.True, element.GetValue(MoveFocusOnEnterProperty));
         }
 
-        /// <summary>
-        /// Helper for setting LoseFocusOnEnter property on a DependencyObject.
-        /// </summary>
-        /// <param name="element">DependencyObject to set LoseFocusOnEnter property on.</param>
+        /// <summary>Helper for setting <see cref="LoseFocusOnEnterProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to set <see cref="LoseFocusOnEnterProperty"/> on.</param>
         /// <param name="value">LoseFocusOnEnter property value.</param>
         public static void SetLoseFocusOnEnter(this DependencyObject element, bool value)
         {
             element.SetValue(LoseFocusOnEnterProperty, value);
         }
 
-        /// <summary>
-        /// Helper for reading LoseFocusOnEnter property from a DependencyObject.
-        /// </summary>
-        /// <param name="element">DependencyObject to read LoseFocusOnEnter property from.</param>
+        /// <summary>Helper for getting <see cref="LoseFocusOnEnterProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to read <see cref="LoseFocusOnEnterProperty"/> from.</param>
         /// <returns>LoseFocusOnEnter property value.</returns>
         [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
         [AttachedPropertyBrowsableForType(typeof(UIElement))]
