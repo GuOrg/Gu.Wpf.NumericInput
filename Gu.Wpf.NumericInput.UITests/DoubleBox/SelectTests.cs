c4ad83ba8f2c3803d593062ce2c86bb64de3deaf
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-30 12:56:57 +0100

    UI-test SelectAllOnFocus.

---
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/SelectTests.cs
@@ -0,0 +1,60 @@
+namespace Gu.Wpf.NumericInput.UITests.DoubleBox
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public static class SelectTests
+    {
+        private const string WindowName = "SelectWindow";
+        private const string ExeFileName = "Gu.Wpf.NumericInput.Demo.exe";
+
+        [SetUp]
+        public static void SetUp()
+        {
+            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
+            var window = app.MainWindow;
+            window.FindTextBox("TextBox2").Text = "2.345";
+            window.FindCheckBox("AllowSpinners").IsChecked = false;
+            window.FindCheckBox("SelectAllOnFocus").IsChecked = false;
+            window.FindCheckBox("SelectAllOnClick").IsChecked = false;
+            window.FindCheckBox("SelectAllOnDoubleClick").IsChecked = false;
+            window.FindCheckBox("MoveFocusOnEnter").IsChecked = false;
+            window.FindCheckBox("LoseFocusOnEnter").IsChecked = false;
+            window.FindTextBox("DigitsBox").Text = "1";
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            Application.KillLaunched(ExeFileName);
+        }
+
+        [Test]
+        public static void SelectAllOnFocus()
+        {
+            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
+            var window = app.MainWindow;
+            window.FindCheckBox("SelectAllOnFocus").IsChecked = true;
+            Assert.AreEqual(string.Empty, window.FindTextBox("TextBox1").SelectedText());
+            window.FindTextBox("TextBox1").Click();
+            Assert.AreEqual("1.234", window.FindTextBox("TextBox1").SelectedText());
+
+            Assert.AreEqual(string.Empty, window.FindTextBox("TextBox2").SelectedText());
+            Keyboard.Type(Key.TAB);
+            Wait.UntilInputIsProcessed();
+            Assert.AreEqual("2.345", window.FindTextBox("TextBox2").SelectedText());
+
+            Assert.AreEqual(string.Empty, window.FindTextBox("TextBox4").SelectedText());
+            window.FindTextBox("TextBox4").Click();
+            Assert.AreEqual("1.234", window.FindTextBox("TextBox4").SelectedText());
+
+            Keyboard.Type(Key.TAB);
+            Wait.UntilInputIsProcessed();
+            Assert.AreEqual("2.345", window.FindTextBox("DoubleBox1").SelectedText());
+
+            Assert.AreEqual(string.Empty, window.FindTextBox("DoubleBox2").SelectedText());
+            window.FindTextBox("DoubleBox2").Click();
+            Assert.AreEqual("3.456", window.FindTextBox("DoubleBox2").SelectedText());
+        }
+    }
+}
--- Gu.Wpf.NumericInput.UITests/Helpers/TextBoxExt.cs
+++ Gu.Wpf.NumericInput.UITests/Helpers/TextBoxExt.cs
@@ -1,5 +1,6 @@
 namespace Gu.Wpf.NumericInput.UITests
 {
+    using System;
     using Gu.Wpf.UiAutomation;
 
     public static class TextBoxExt
@@ -7,6 +8,20 @@ namespace Gu.Wpf.NumericInput.UITests
         public static TextBlock FormattedView(this TextBox baseBox)
         {
             return baseBox.FindTextBlock("PART_FormattedText");
+        }
+
+        public static string SelectedText(this TextBox baseBox)
+        {
+            var selection = baseBox.TextPattern.GetSelection();
+            switch (selection.Length)
+            {
+                case 0:
+                    return null;
+                case 1:
+                    return selection[0].GetText(int.MaxValue);
+                default:
+                    throw new InvalidOperationException();
+            }
         }
     }
 }
