4d9dc9d2986b1273aae91f960d3d2a883c80cc2a
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-30 14:59:50 +0100

    Remove unused.

--- Gu.Wpf.NumericInput/Internals/FrameworkObject.cs
+++ Gu.Wpf.NumericInput/Internals/FrameworkObject.cs
@@ -3,7 +3,6 @@ namespace Gu.Wpf.NumericInput
 {
     using System;
     using System.Windows;
-    using System.Windows.Markup;
     using System.Windows.Media;
     using System.Windows.Media.Media3D;
 
@@ -36,21 +35,6 @@ namespace Gu.Wpf.NumericInput
             }
         }
 
-        internal FrameworkObject(FrameworkElement frameworkElement, FrameworkContentElement frameworkContentElement)
-        {
-            this.FrameworkElement = frameworkElement;
-            this.FrameworkContentElement = frameworkContentElement;
-
-            if (frameworkElement != null)
-            {
-                this.DependencyObject = frameworkElement;
-            }
-            else
-            {
-                this.DependencyObject = frameworkContentElement;
-            }
-        }
-
         internal FrameworkElement FrameworkElement { get; private set; }
 
         internal FrameworkContentElement FrameworkContentElement { get; private set; }
@@ -60,116 +44,6 @@ namespace Gu.Wpf.NumericInput
         internal bool IsFE => this.FrameworkElement != null;
 
         internal bool IsFCE => this.FrameworkContentElement != null;
-
-        internal bool IsValid => this.FrameworkElement != null || this.FrameworkContentElement != null;
-
-        //// logical parent
-        internal DependencyObject Parent
-        {
-            get
-            {
-                if (this.IsFE)
-                {
-                    return this.FrameworkElement.Parent;
-                }
-                else if (this.IsFCE)
-                {
-                    return this.FrameworkContentElement.Parent;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-        }
-
-        internal DependencyObject TemplatedParent
-        {
-            get
-            {
-                if (this.IsFE)
-                {
-                    return this.FrameworkElement.TemplatedParent;
-                }
-                else if (this.IsFCE)
-                {
-                    return this.FrameworkContentElement.TemplatedParent;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-        }
-
-        //// internal Style ThemeStyle
-        //// {
-        ////    get
-        ////    {
-        ////        if (this.IsFE)
-        ////        {
-        ////            return this._fe.ThemeStyle;
-        ////        }
-        ////        else if (this.IsFCE)
-        ////        {
-        ////            return this._fce.ThemeStyle;
-        ////        }
-        ////        else
-        ////        {
-        ////            return null;
-        ////        }
-        ////    }
-        //// }
-
-        internal XmlLanguage Language
-        {
-            get
-            {
-                if (this.IsFE)
-                {
-                    return this.FrameworkElement.Language;
-                }
-                else if (this.IsFCE)
-                {
-                    return this.FrameworkContentElement.Language;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-        }
-
-        internal Style Style
-        {
-            get
-            {
-                if (this.IsFE)
-                {
-                    return this.FrameworkElement.Style;
-                }
-                else if (this.IsFCE)
-                {
-                    return this.FrameworkContentElement.Style;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-
-            set
-            {
-                if (this.IsFE)
-                {
-                    this.FrameworkElement.SetCurrentValue(FrameworkElement.StyleProperty, value);
-                }
-                else if (this.IsFCE)
-                {
-                    this.FrameworkContentElement.SetCurrentValue(FrameworkContentElement.StyleProperty, value);
-                }
-            }
-        }
 
         // returns the effective parent, whether visual, logical,
         // inheritance context, etc.
@@ -217,25 +91,6 @@ namespace Gu.Wpf.NumericInput
             }
         }
 
-        internal bool IsInitialized
-        {
-            get
-            {
-                if (this.IsFE)
-                {
-                    return this.FrameworkElement.IsInitialized;
-                }
-                else if (this.IsFCE)
-                {
-                    return this.FrameworkContentElement.IsInitialized;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-        }
-
         internal static bool IsEffectiveAncestor(DependencyObject d1, DependencyObject d2)
         {
             for (var fo = new FrameworkObject(d2);
@@ -249,37 +104,6 @@ namespace Gu.Wpf.NumericInput
             }
 
             return false;
-        }
-
-        internal static FrameworkObject GetContainingFrameworkElement(DependencyObject current)
-        {
-            var fo = new FrameworkObject(current);
-
-            while (!fo.IsValid && fo.DependencyObject != null)
-            {
-                // The current object is neither a FrameworkElement nor a
-                // FrameworkContentElement.  We will now walk the "core"
-                // tree looking for one.
-                if (fo.DependencyObject is Visual visual)
-                {
-                    fo.Reset(VisualTreeHelper.GetParent(visual));
-                }
-                else if (fo.DependencyObject is ContentElement ce)
-                {
-                    fo.Reset(ContentOperations.GetParent(ce));
-                }
-                else if (fo.DependencyObject is Visual3D visual3D)
-                {
-                    fo.Reset(VisualTreeHelper.GetParent(visual3D));
-                }
-                else
-                {
-                    // The parent could be an application.
-                    fo.Reset(null);
-                }
-            }
-
-            return fo;
         }
 
         internal void Reset(DependencyObject d)
@@ -300,18 +124,6 @@ namespace Gu.Wpf.NumericInput
             {
                 this.FrameworkElement = null;
                 this.FrameworkContentElement = null;
-            }
-        }
-
-        internal void RaiseEvent(RoutedEventArgs args)
-        {
-            if (this.IsFE)
-            {
-                this.FrameworkElement.RaiseEvent(args);
-            }
-            else if (this.IsFCE)
-            {
-                this.FrameworkContentElement.RaiseEvent(args);
             }
         }
 
