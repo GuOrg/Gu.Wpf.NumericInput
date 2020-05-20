62abeae4034c433a80febedd8941b5821eb85015
JohanLarsson <johan_gavle1@hotmail.com>
2020-05-20 22:05:25 +0200

    Gu.Wpf.NumericInput.Material.csproj
    No repro #36

---
+++ Gu.Wpf.NumericInput.Material/App.xaml.cs
@@ -0,0 +1,8 @@
+namespace Gu.Wpf.NumericInput.Material
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+    }
+}
---
+++ Gu.Wpf.NumericInput.Material/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+namespace Gu.Wpf.NumericInput.Material
+{
+    using System.Windows;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
