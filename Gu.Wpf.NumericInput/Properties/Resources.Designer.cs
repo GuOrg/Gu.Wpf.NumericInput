6241883e0bc5f96f58d8ee0e8c3024db9dd61dfb
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-06 09:11:13 +0100

    Annotate

--- Gu.Wpf.NumericInput/Properties/Resources.Designer.cs
+++ Gu.Wpf.NumericInput/Properties/Resources.Designer.cs
@@ -19,10 +19,10 @@ namespace Gu.Wpf.NumericInput.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,10 +36,10 @@ namespace Gu.Wpf.NumericInput.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    var temp = new global::System.Resources.ResourceManager("Gu.Wpf.NumericInput.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gu.Wpf.NumericInput.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -51,7 +51,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Please enter a number..
         /// </summary>
-        public static string Please_enter_a_number {
+        internal static string Please_enter_a_number {
             get {
                 return ResourceManager.GetString("Please_enter_a_number", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Please enter a valid number..
         /// </summary>
-        public static string Please_enter_a_valid_number {
+        internal static string Please_enter_a_valid_number {
             get {
                 return ResourceManager.GetString("Please_enter_a_valid_number", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Please enter a value between {0} and {1}..
         /// </summary>
-        public static string Please_enter_a_value_between__0__and__1__ {
+        internal static string Please_enter_a_value_between__0__and__1__ {
             get {
                 return ResourceManager.GetString("Please_enter_a_value_between__0__and__1__", resourceCulture);
             }
@@ -90,7 +90,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Please enter a value greater than or equal to {0}..
         /// </summary>
-        public static string Please_enter_a_value_greater_than_or_equal_to__0__ {
+        internal static string Please_enter_a_value_greater_than_or_equal_to__0__ {
             get {
                 return ResourceManager.GetString("Please_enter_a_value_greater_than_or_equal_to__0__", resourceCulture);
             }
@@ -99,7 +99,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Please enter a value less than or equal to {0}..
         /// </summary>
-        public static string Please_enter_a_value_less_than_or_equal_to__0__ {
+        internal static string Please_enter_a_value_less_than_or_equal_to__0__ {
             get {
                 return ResourceManager.GetString("Please_enter_a_value_less_than_or_equal_to__0__", resourceCulture);
             }
@@ -108,7 +108,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Please provide valid input..
         /// </summary>
-        public static string Please_provide_valid_input {
+        internal static string Please_provide_valid_input {
             get {
                 return ResourceManager.GetString("Please_provide_valid_input", resourceCulture);
             }
@@ -117,7 +117,7 @@ namespace Gu.Wpf.NumericInput.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Syntax error in regex pattern..
         /// </summary>
-        public static string Syntax_error_in_regex_pattern {
+        internal static string Syntax_error_in_regex_pattern {
             get {
                 return ResourceManager.GetString("Syntax_error_in_regex_pattern", resourceCulture);
             }
